9af9f8d39ee147e66aa065a7d9a211f5eb5e8671
ekwls20 <ekwls20@gmail.com>
2025-07-25 17:25:25 +0900

    Comment(#25) : 테스트 겸 주석문 추가

--- src/main/java/cowing/project/cowingmsatrading/global/exception/OrderPendingException.java
+++ src/main/java/cowing/project/cowingmsatrading/global/exception/OrderPendingException.java
@@ -1,5 +1,6 @@
 package cowing.project.cowingmsatrading.global.exception;
 
+// 이것은 OrderPendingException입니다
 public class OrderPendingException extends RuntimeException {
     public OrderPendingException(String message) {
         super(message);
