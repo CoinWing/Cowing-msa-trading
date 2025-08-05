425c558d46b801d35b2c3831dc11b7e2b984e9fc
ekwls20 <ekwls20@gmail.com>
2025-08-06 00:28:55 +0900

    Feat(#36): find Order by UUID 메소드 추가

--- src/main/java/cowing/project/cowingmsatrading/trade/domain/repository/OrderRepository.java
+++ src/main/java/cowing/project/cowingmsatrading/trade/domain/repository/OrderRepository.java
@@ -5,7 +5,10 @@ import cowing.project.cowingmsatrading.trade.domain.entity.order.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByUsernameAndStatus(String username, Status status);
+
+    Optional<Order> findByUuid(String uuid);
 }
