b203e200aa41587bc1013ba76f757aae93359510
ekwls20 <ekwls20@gmail.com>
2025-08-06 00:31:34 +0900

    Refactorˆ(#36): 사용하는 어노테이션만 명시 및 사용하지 않는 Import 삭제

--- src/main/java/cowing/project/cowingmsatrading/trade/controller/OrderController.java
+++ src/main/java/cowing/project/cowingmsatrading/trade/controller/OrderController.java
@@ -2,13 +2,15 @@ package cowing.project.cowingmsatrading.trade.controller;
 
 import cowing.project.cowingmsatrading.trade.dto.PendingOrderData;
 import cowing.project.cowingmsatrading.trade.service.processor.PendingOrderProcessor;
-import cowing.project.cowingmsatrading.trade.service.processor.PendingOrderStreamManager;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
